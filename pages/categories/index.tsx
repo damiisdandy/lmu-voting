72384e7fce564f2f51c81ccc2fb703c1ecaa7935
Damilola Jerugba <damiisdandy@icloud.com>
2022-05-23 08:59:32 +0100

    sort with created at

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -32,6 +32,7 @@ const QUERY = gql`
         name
         slug
         isVoted
+        createdAt
         votedFor {
           name
         }
@@ -119,59 +120,62 @@ const Categories = () => {
             }}
             gap="7"
           >
-            {data?.sessionBySlug.categories.map((category) => (
-              <MotionGridItem
-                onClick={() => push(`/categories/${category.slug}`)}
-                variants={variantItem}
-                key={category.id}
-              >
-                <Box
-                  border="2px solid transparent"
-                  borderColor="brand.100"
-                  // bgColor="brand.100"
-                  borderRadius="md"
-                  p={{ base: "3", md: "4" }}
-                  cursor="pointer"
-                  transition="all 200ms ease"
-                  sx={category.isVoted ? ACTIVE_STYLE : {}}
-                  _hover={ACTIVE_STYLE}
+            {data?.sessionBySlug.categories
+              // @ts-ignore
+              .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+              .map((category) => (
+                <MotionGridItem
+                  onClick={() => push(`/categories/${category.slug}`)}
+                  variants={variantItem}
+                  key={category.id}
                 >
-                  <Heading
-                    mb="4"
-                    fontSize={{ base: "25px", md: "27px", lg: "35px" }}
+                  <Box
+                    border="2px solid transparent"
+                    borderColor="brand.100"
+                    // bgColor="brand.100"
+                    borderRadius="md"
+                    p={{ base: "3", md: "4" }}
+                    cursor="pointer"
+                    transition="all 200ms ease"
+                    sx={category.isVoted ? ACTIVE_STYLE : {}}
+                    _hover={ACTIVE_STYLE}
                   >
-                    {category.name}
-                  </Heading>
-                  <HStack justify="space-between">
-                    <AvatarGroup
-                      spacing="-2"
-                      size="sm"
-                      max={3}
-                      sx={{
-                        ".chakra-avatar__excess": {
-                          bgColor: "transparent",
-                          fontWeight: 900,
-                          fontSize: "12px",
-                          borderColor: "brand.100",
-                          ml: "-5px",
-                          color: "black",
-                        },
-                      }}
+                    <Heading
+                      mb="4"
+                      fontSize={{ base: "25px", md: "27px", lg: "35px" }}
                     >
-                      {category.nominees.map((nominee) => (
-                        <Avatar
-                          className="avatar"
-                          borderColor="brand.100"
-                          key={nominee.id}
-                          src={config.apiUrl + "/" + nominee.picture}
-                        />
-                      ))}
-                    </AvatarGroup>
-                    <Button className="btn">Browse</Button>
-                  </HStack>
-                </Box>
-              </MotionGridItem>
-            ))}
+                      {category.name}
+                    </Heading>
+                    <HStack justify="space-between">
+                      <AvatarGroup
+                        spacing="-2"
+                        size="sm"
+                        max={3}
+                        sx={{
+                          ".chakra-avatar__excess": {
+                            bgColor: "transparent",
+                            fontWeight: 900,
+                            fontSize: "12px",
+                            borderColor: "brand.100",
+                            ml: "-5px",
+                            color: "black",
+                          },
+                        }}
+                      >
+                        {category.nominees.map((nominee) => (
+                          <Avatar
+                            className="avatar"
+                            borderColor="brand.100"
+                            key={nominee.id}
+                            src={config.apiUrl + "/" + nominee.picture}
+                          />
+                        ))}
+                      </AvatarGroup>
+                      <Button className="btn">Browse</Button>
+                    </HStack>
+                  </Box>
+                </MotionGridItem>
+              ))}
           </MotionGrid>
         )}
       </Box>
