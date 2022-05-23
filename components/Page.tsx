a2f9552e47574673141a1324eb4168c1fc985230
Damilola Jerugba <damiisdandy@icloud.com>
2022-05-23 16:18:51 +0100

    removed import

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import React from "react";
-import { useGlobalContext } from "../context";
 import Layout from "./Layout";
 import WithDefaultGlobalStyles from "./WithDefaultGlobalStyles";
 
